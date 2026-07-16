d3cccd73b5cf72e48f34957b50dc77cd71dc4dac
bonbondli <bonbondli@gmail.com>
2026-07-16 13:01:03 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/tro-choi-tieng-anh/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
